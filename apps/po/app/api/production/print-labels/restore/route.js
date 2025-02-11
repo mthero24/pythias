e960d9328796999e19731e859d8c80e3c01ce59a
Michael Thero <mthero24@gmail.com>
2025-02-11 06:31:05 -0500

    something

--- apps/po/app/api/production/print-labels/restore/route.js
+++ apps/po/app/api/production/print-labels/restore/route.js
@@ -43,7 +43,7 @@ export async function POST(req=NextApiResponse) {
     let headers = {
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer $2a$10$HN2gb5EVudQkf9op49kKJu3MoQQlPMgN00XUMIoy51EIiHNtbZqNm`,
+        Authorization: `Bearer $2a$10$PDlV9Xhf.lMicHvMvBCMwuyCYUhWGqjaCEFpG0AJMSKteUfKBO.Hy`,
       },
     };
     console.log(headers)
