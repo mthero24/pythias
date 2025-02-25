28fcca0fdae00545ddb80b3e6df90b011489a608
Michael Thero <mthero24@gmail.com>
2025-02-25 09:12:30 -0700

    something

--- apps/premier-printing/app/test/page.js
+++ apps/premier-printing/app/test/page.js
@@ -16,51 +16,51 @@ export default async function Test(){
     //     }
     // }
     // console.log("found: ", skusFound, "Not Found: ", skusNotFOund)
-    let colors = await Color.find({}).lean()
-    let skus = await SkuToUpc.find({design: null})
-    console.log(skus)
-    let sizes = ["Small", "Large", "Medium", "XSmall", "XLarge", "2XLarge", "XS", "S", "L", "M", "XL", "2XL"]
-    for(let s of skus){
-        console.log(s)
-        // if(s.sku.split("_")[0] == "R"){
-        //     s.blank = await Blank.findOne({code: "LSO"})
-        //     await s.save()
-        // } 
-        // if(s.sku.split("_")[0] == "PPSET") {
-        //     s.blank = await Blank.findOne({code: "PPSET_C"})
-        //     await s.save()
-        // }
-        // if(s.sku.split("_")[0] == "MGDT") {
-        //     s.blank = await Blank.findOne({code: "GDT"})
-        //     await s.save()
-        // }
-        // else if(s.sku == "" || s.sku.split("_")[0] == "LS" || s.sku.split("_")[0] == "O"|| s.sku.split("_")[0] == "MUG" || s.sku.split("_")[0] == "BCSWT"|| s.sku.split("_")[0] == "HAT" || s.sku.split("_")[0] == "V"){
-        //     try{
-        //         await SkuToUpc.findOneAndDelete({_id: s._id})
-        //     }catch(e){
-        //         console.log(e)
-        //     }
-        // }
-        // if(sizes.includes(s.sku.split("_")[1])){
-        //     s.size = s.sku.split("_")[1]
-        //     s.color = colors.filter(c=> s.sku.split("_")[2].replace(/\./g, "").replace(/ /g, "").toLowerCase() == c.name.replace(/\./g, "").replace(/ /g, "").toLowerCase())[0]//await Color.findOne({name: s.sku.split("_")[1]})
-        // }else{
-        //     s.color = colors.filter(c=> s.sku.split("_")[1].replace(/\./g, "").replace(/ /g, "").toLowerCase() == c.name.replace(/\./g, "").replace(/ /g, "").toLowerCase())[0]//await Color.findOne({name: s.sku.split("_")[1]})
-        //     s.size = s.sku.split("_")[2]
-        // }
-        let dSku = s.sku.split("_").slice(3)
-        //console.log(dSku)
-        if(dSku[0]?.includes("MC")){
-            let d_sku = ''
-            for(let i = 0; i < dSku.length; i++){
-                if(i == 0) d_sku = d_sku + dSku[i].replace("MC", "M")
-                else d_sku = `${d_sku}_${dSku[i]}`
-            }
-            console.log(d_sku)
-            s.design = await Design.findOne({sku: d_sku})
-            await s.save()
-        }
-        // console.log(s)
-    }
+    // let colors = await Color.find({}).lean()
+    // let skus = await SkuToUpc.find({design: null})
+    // console.log(skus)
+    // let sizes = ["Small", "Large", "Medium", "XSmall", "XLarge", "2XLarge", "XS", "S", "L", "M", "XL", "2XL"]
+    // for(let s of skus){
+    //     console.log(s)
+    //     // if(s.sku.split("_")[0] == "R"){
+    //     //     s.blank = await Blank.findOne({code: "LSO"})
+    //     //     await s.save()
+    //     // } 
+    //     // if(s.sku.split("_")[0] == "PPSET") {
+    //     //     s.blank = await Blank.findOne({code: "PPSET_C"})
+    //     //     await s.save()
+    //     // }
+    //     // if(s.sku.split("_")[0] == "MGDT") {
+    //     //     s.blank = await Blank.findOne({code: "GDT"})
+    //     //     await s.save()
+    //     // }
+    //     // else if(s.sku == "" || s.sku.split("_")[0] == "LS" || s.sku.split("_")[0] == "O"|| s.sku.split("_")[0] == "MUG" || s.sku.split("_")[0] == "BCSWT"|| s.sku.split("_")[0] == "HAT" || s.sku.split("_")[0] == "V"){
+    //     //     try{
+    //     //         await SkuToUpc.findOneAndDelete({_id: s._id})
+    //     //     }catch(e){
+    //     //         console.log(e)
+    //     //     }
+    //     // }
+    //     // if(sizes.includes(s.sku.split("_")[1])){
+    //     //     s.size = s.sku.split("_")[1]
+    //     //     s.color = colors.filter(c=> s.sku.split("_")[2].replace(/\./g, "").replace(/ /g, "").toLowerCase() == c.name.replace(/\./g, "").replace(/ /g, "").toLowerCase())[0]//await Color.findOne({name: s.sku.split("_")[1]})
+    //     // }else{
+    //     //     s.color = colors.filter(c=> s.sku.split("_")[1].replace(/\./g, "").replace(/ /g, "").toLowerCase() == c.name.replace(/\./g, "").replace(/ /g, "").toLowerCase())[0]//await Color.findOne({name: s.sku.split("_")[1]})
+    //     //     s.size = s.sku.split("_")[2]
+    //     // }
+    //     // let dSku = s.sku.split("_").slice(3)
+    //     // //console.log(dSku)
+    //     // if(dSku[0]?.includes("MC")){
+    //     //     let d_sku = ''
+    //     //     for(let i = 0; i < dSku.length; i++){
+    //     //         if(i == 0) d_sku = d_sku + dSku[i].replace("MC", "M")
+    //     //         else d_sku = `${d_sku}_${dSku[i]}`
+    //     //     }
+    //     //     console.log(d_sku)
+    //     //     s.design = await Design.findOne({sku: d_sku})
+    //     //     await s.save()
+    //     // }
+    //     // console.log(s)
+    // }
     return <h1>test</h1>
 }
