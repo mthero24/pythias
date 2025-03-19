fcf4f1e2e6200fef144d6130f2c99c59ef5add01
Michael Thero <mthero24@gmail.com>
2025-03-18 20:46:13 -0400

    something

--- apps/premier-printing/app/api/renderImages/route.js
+++ apps/premier-printing/app/api/renderImages/route.js
@@ -56,7 +56,7 @@ const createImage = async (data)=>{
             {
                 input: designBase64,
                 blend: 'atop',
-                top: parseInt(data.box.y * 1.75) + (data.box.rotation? data.box.rotation < 0? parseInt(3.1415926535 * data.box.rotation): -parseInt(3.1415926535 * data.box.rotation) : 0),
+                top: parseInt(data.box.y * 1.75) + (data.box.rotation? data.box.rotation < 0? parseInt(data.box.rotation): -parseInt(data.box.rotation) : 0),
                 left: parseInt(data.box.x * 1.75) + (offset? offset: 0),
                 gravity: "center",
             },
