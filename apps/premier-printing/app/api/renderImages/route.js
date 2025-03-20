fbdad81ec764e6b771c4867ab03820089816c724
Michael Thero <mthero24@gmail.com>
2025-03-19 20:23:07 -0400

    something

--- apps/premier-printing/app/api/renderImages/route.js
+++ apps/premier-printing/app/api/renderImages/route.js
@@ -36,6 +36,9 @@ const createImage = async (data)=>{
             console.log(data.box.rotation, "rotation")
             designBase64 = designBase64.rotate(parseInt(data.box.rotation), {background: {r: 0, g: 0, b: 0, alpha: 0}})
         }
+        designBase64 = await designBase64.toBuffer();
+        designBase64 = await sharp(designBase64)
+        const metadata2 = await designBase64.metadata()
         designBase64 = await designBase64.resize({
             width: parseInt(data.box.boxWidth * 1.75),
             height: parseInt(data.box.boxHeight * 1.75),
@@ -52,9 +55,6 @@ const createImage = async (data)=>{
         //     designBase64 = designBase64.rotate(parseInt(data.box.rotation), {background: {r: 0, g: 0, b: 0, alpha: 0}})
         // }
         console.log(data.box)
-        designBase64 = await designBase64.toBuffer();
-        designBase64 = await sharp(designBase64)
-        const metadata2 = await designBase64.metadata()
         designBase64 = await designBase64.toBuffer();
         console.log(metadata.width, 'meta', metadata2.width, 'meta2', parseInt(data.box.boxWidth * 1.75), "box")
         let offset = parseInt(((data.box.boxWidth * 1.75) - (metadata2.width)) / 2)
