e086be85ac0fcb96e699e662e0538576b51cf214
Michael Thero <mthero24@gmail.com>
2025-03-19 21:53:43 -0400

    something

--- apps/premier-printing/app/api/renderImages/route.js
+++ apps/premier-printing/app/api/renderImages/route.js
@@ -60,7 +60,7 @@ const createImage = async (data)=>{
         if(data.box.rotation){
             let radians = data.box.rotation * (Math.PI / 180)
             let newX = (x * Math.cos(radians)) - (y * Math.sin(radians))
-            let newY = (x * Math.sin(radians)) - (y * Math.cos(radians))
+            let newY = (x * Math.sin(radians)) + (y * Math.cos(radians))
             x= newX;
             y=newY
             console.log(x, "x", y, "y")
