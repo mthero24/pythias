55933ca1feda86548c5da6c95061e5e13b187319
Michael Thero <mthero24@gmail.com>
2025-03-27 12:56:49 -0400

    something

--- apps/premier-printing/app/production/returns/page.js
+++ apps/premier-printing/app/production/returns/page.js
@@ -1,6 +1,7 @@
 import { serialize } from "@/functions/serialize"
 import Bins from "@/models/returnBins"
 import {Main} from "@pythias/returns"
+export const dynamic = 'force-dynamic';
 export default async function Returns(){
     let binsInUse = await Bins.find({inUse: true}).populate("design", "sku images").populate("blank", "sizes code multiImages").populate("color", "name")
     let binCount = await Bins.find().countDocuments()
--- apps/premier-printing/app/test/page.js
+++ apps/premier-printing/app/test/page.js
@@ -17,6 +17,7 @@ export default async function Test(){
     //let res = await axios.get(`https://api.gs1us.org/api/v1/myproduct/${g}`, headers).catch(e=> console.log(e.response?.data))
     let designs = await Design.find({published: true}).populate("brands b2m blanks.blank blanks.colors blanks.defaultColor").sort({'_id': -1})
     let brands = {}
+    let variants = {}
     let bl = 0
     console.log(designs.length)
     for(let design of designs){
@@ -24,30 +25,62 @@ export default async function Test(){
         if(design.blanks.length > 0){
             for(let brand of design.brands){
                // console.log(brand)
-                if(!brands[brand.name]) brands[brand.name] = []
-                for(let b of design.blanks){
-                    //console.log(`${brand.name} ${design.name} ${b.blank.name}`, brand.name, `${design.description} ${b.blank.description}`)
-                    let product = {
-                        name: `${brand.name} ${design.name} ${b.blank.name}`,
-                        brand: brand.name,
-                        description: `${design.description} ${b.blank.description}`,
-                        options: ["color", "size"],
-                        
+                //console.log(design.b2m.filter(b2m=> b2m.brand == brand.name)[0])
+                let b2m = design.b2m.filter(b2m=> b2m.brand == brand.name)[0]
+                if(b2m){
+                    console.log(b2m)
+                    if(!brands[brand.name]) brands[brand.name] = {}
+                    for(let m of b2m.marketPlaces){
+                        let marketplace = m
+                        if(m == "Shien") marketplace = "Shein"
+                        if(m == "shopify") marketplace = "Shopify"
+                        if(m == "amazon") marketplace = "Amazon"
+                        if(!brands[brand.name][marketplace]) brands[brand.name][marketplace] = []
+                        console.log(brands[brand.name][marketplace].length)
                     }
-                    brands[brand.name].push(product)
-                    // for(let c of b.colors){
-                    //     for(let s of b.blank.sizes){
-                    //         console.log(design.sku, b.blank.code, c.name, s.name)
-                    //         // name brand 
-                    //     }
-                    // }
+                    for(let b of design.blanks){
+                        //console.log(`${brand.name} ${design.name} ${b.blank.name}`, brand.name, `${design.description} ${b.blank.description}`)
+                        let variants = []
+                         for(let c of b.colors){
+                            for(let s of b.blank.sizes){
+                                let upc = await SkuToUpc.findOne({design: design._id, blank: b.blank._id, color: c._id, size: s.name})
+                                let v = {
+                                    upc: upc?.upc,
+                                    sku: upc?.sku,
+                                    gtin: upc?.gtin,
+                                    size: s,
+                                    color: c
+                                }
+
+                            }
+                        }
+                        let product = {
+                            name: `${brand.name} ${design.name} ${b.blank.name}`,
+                            brand: brand.name,
+                            design: design,
+                            blank: b,
+                            options: ["color", "size"],
+                            variants: variants
+
+                            
+                        }
+                        for(let m of b2m.marketPlaces){
+                            let marketplace = m
+                            if(m == "Shien") marketplace = "Shein"
+                            if(m == "shopify") marketplace = "Shopify"
+                            if(m == "amazon") marketplace = "Amazon"
+                            brands[brand.name][marketplace].push(product)
+                        }
+                    }
                 }
             }
         }
     }
     //console.log(brands)
     Object.keys(brands).map(b=>{
-        console.log(brands[b].length, b)
+       Object.keys(brands[b]).map(m=>{
+            console.log(b, m, brands[b][m].length)
+       })
     })
     //await SkuToUpc.updateMany({recycle: true, blank: {$ne: null}}, {recycle: false})
     return <h1>test</h1>
