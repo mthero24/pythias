e1f628bdf0350e07a21db4820734b70b34990374
Michael Thero <mthero24@gmail.com>
2025-04-25 12:27:31 -0400

    something

--- apps/premier-printing/app/admin/blanks/[id]/page.js
+++ apps/premier-printing/app/admin/blanks/[id]/page.js
@@ -1,6 +1,7 @@
 import Blank from "@/models/Blanks";
 import { serialize } from "@/functions/serialize";
 import {Main} from "./Main";
+export const dynamic = 'force-dynamic'; 
 export default async function Show(req, res){
     //console.log(await req.params);
     let {id} = await req.params;
