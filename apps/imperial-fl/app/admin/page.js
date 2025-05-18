99a9bdc28273e85d6019967c5841a981cb69100d
Michael Thero <mthero24@gmail.com>
2025-05-17 22:51:51 -0400

    something

--- apps/imperial-fl/app/admin/page.js
+++
@@ -1,5 +0,0 @@
-
-
-export default function Admin(){
-    return(<h1 style={{fontSize: "3rem", textAlign: "center"}}>Admin Page</h1>)
-}
--- apps/imperial-fl/app/admin/register/page.js
+++
@@ -1,7 +0,0 @@
-import {Main} from "@/app/login/Main"
-
-export default async function Login(){
-    return(
-        <Main type="register"/>
-    )
-}
