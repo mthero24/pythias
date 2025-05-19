5a8db0f96b894057b9d6774cbadf9d04d55af963
Michael Thero <mthero24@gmail.com>
2025-05-19 15:07:29 -0400

    something

--- apps/premier-printing/app/api/production/roq-folder/route.js
+++ apps/premier-printing/app/api/production/roq-folder/route.js
@@ -23,7 +23,7 @@ export async function POST(req = NextApiRequest){
                 address: item.order.shippingAddress, 
                 poNumber: item.order.poNumber, 
                 weight: item.blank.sizes.filter(s=> s.name.toLowerCase() == item.sizeName.toLowerCase())[0].weight?item.blank.sizes.filter(s=> s.name.toLowerCase() == item.sizeName.toLowerCase())[0].weight: 8, 
-                selectedShipping: {provider: "usps", name: "USPS_Ground_Advantage"}, dimensions: {width: 8, length: 11, height: 1}, 
+                selectedShipping: {provider: "usps", name: "USPS_GROUND_ADVANTAGE"}, dimensions: {width: 8, length: 11, height: 1}, 
                 businessAddress: JSON.parse(process.env.businessAddress),
                 providers: ["usps", "ups"],                
                 credentials: {
