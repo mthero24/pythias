443f6d7b684035c46508142ba6f2bb896a8810e9
Michael Thero <mthero24@gmail.com>
2025-06-19 12:52:12 -0600

    something

--- apps/premier-printing/app/api/integrations/shopify/brands/route.js
+++ apps/premier-printing/app/api/integrations/shopify/brands/route.js
@@ -7,19 +7,21 @@ export async function GET(req=NextApiRequest){
      const headersList = await headers();
     const authorizationHeader = headersList.get("authorization");
     console.log("Authorization:", authorizationHeader);
-    let password = authorizationHeader.split(" ")[1]
-    console.log(password)
-    let user = await User.findOne({password: password})
-    console.log(user)
-    if(user){
-        let filtered = []
-        let brands = await Brands.find({})
-        for(let b of brands){
-            if(!filtered.filter(br=> br.name == b.name)[0]){
-                filtered.push(b)
+    if(authorizationHeader){
+        let password = authorizationHeader?.split(" ")[1]
+        console.log(password)
+        let user = await User.findOne({password: password})
+        console.log(user)
+        if(user){
+            let filtered = []
+            let brands = await Brands.find({})
+            for(let b of brands){
+                if(!filtered.filter(br=> br.name == b.name)[0]){
+                    filtered.push(b)
+                }
             }
+            return NextResponse.json({error: false, brands: filtered})
         }
-        return NextResponse.json({error: false, brands: filtered})
     }
     return NextResponse.json({error: true, msg: "Unauthorized"})
 }
