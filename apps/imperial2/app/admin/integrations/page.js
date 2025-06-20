4b0fad4a5c4919c0e881ef875a616255a82d6c06
Michael Thero <mthero24@gmail.com>
2025-06-20 16:09:13 -0600

    something

--- apps/imperial2/app/admin/integrations/page.js
+++ apps/imperial2/app/admin/integrations/page.js
@@ -2,7 +2,8 @@ import {Main} from "@pythias/integrations";
 import TikTokAuth from "@/models/tiktok";
 import { serialize } from "@/functions/serialize";
 export default async function Integrations(){
-    let tiktokshops = await TikTokAuth.find({provider: "premierPrinting"})
-    tiktokShops = serialize(tiktokshops)
+    let tiktokshops
+    //let tiktokshops = await TikTokAuth.find({provider: "premierPrinting"})
+    //tiktokShops = serialize(tiktokshops)
     return <Main tiktokShops={tiktokShops}/>
 }
