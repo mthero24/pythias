71103723b44e4c409acb970996f49702622723e5
Michael Thero <mthero24@gmail.com>
2025-06-24 08:40:05 -0600

    something

--- apps/premier-printing/app/test/page.js
+++ apps/premier-printing/app/test/page.js
@@ -1,13 +1,13 @@
 import TikTokAuth from "@/models/tiktok"
 import {getAuthorizedShops, getAccessTokenFromRefreshToken} from "@pythias/integrations"
 export default async function Test(){
-    let credentials = await TikTokAuth.findOne({provider: "premierPrinting"})
-    let shop = await getAuthorizedShops(credentials)
-    if(shop.error && shop.msg == "refresh"){
-        let access_token = getAccessTokenFromRefreshToken(credentials.refresh_token)
-        credentials.access_token = access_token
-        await credentials.save()
-        shop = await getAuthorizedShops(credentials)
-    } 
+//     let credentials = await TikTokAuth.findOne({provider: "premierPrinting"})
+//     let shop = await getAuthorizedShops(credentials)
+//     if(shop.error && shop.msg == "refresh"){
+//         let access_token = getAccessTokenFromRefreshToken(credentials.refresh_token)
+//         credentials.access_token = access_token
+//         await credentials.save()
+//         shop = await getAuthorizedShops(credentials)
+//     } 
     return <h1>test</h1>
 }
