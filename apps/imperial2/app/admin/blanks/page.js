5b65557566e2d673867fb86c6080562af8359569
Michael Thero <mthero24@gmail.com>
2025-07-03 20:59:00 -0400

    somehting

--- apps/imperial2/app/admin/blanks/page.js
+++ apps/imperial2/app/admin/blanks/page.js
@@ -3,8 +3,9 @@ import Blank from "@/models/Blanks";
 export const dynamic = 'force-dynamic'; 
 export default async function Blanks(){
     let blanks = await Blank.find({}).select("code name vendor department sales _id").lean().catch(e=>{console.log(e)});
+    console.log(blanks)
     if(blanks)blanks = JSON.parse(JSON.stringify(blanks))
-      else blanks = []
+    else blanks = []
     return (
       <Main blanks={blanks}/>
     )
