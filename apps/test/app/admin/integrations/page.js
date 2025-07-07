75829f28acf846d4d4cc76a6e1f1a840f89d7327
michael thero <mthero24@gmail.com>
2025-07-07 06:30:39 -0400

    something

--- apps/test/app/admin/integrations/page.js
+++ apps/test/app/admin/integrations/page.js
@@ -3,8 +3,8 @@ import TikTokAuth from "@/models/tiktok";
 import { serialize } from "@/functions/serialize";
 export const dynamic = 'force-dynamic';
 export default async function Integrations(){
-    let tiktokShops = await TikTokAuth.find({provider: "premierPrinting"})
+    let tiktokShops = await TikTokAuth.find({provider: "test"})
     console.log(tiktokShops)
     tiktokShops = serialize(tiktokShops)
-    return <Main tiktokShops={tiktokShops} provider={"premierPrinting"}/>
+    return <Main tiktokShops={tiktokShops} provider={"test"}/>
 }
