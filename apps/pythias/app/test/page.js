361ac58722b6a869c4b4b0ade929e835d008e9bb
michael thero <mthero24@gmail.com>
2025-07-07 05:46:39 -0400

    something

--- apps/pythias/app/test/page.js
+++ apps/pythias/app/test/page.js
@@ -2,7 +2,7 @@ import TikTokAuth from "@/models/tiktok"
 
 export default async function Test(){
     console.log("test ++++++++++++++++++++++++")
-    let tikToks = await TikTokAuth.find({})
+    let tikToks = await TikTokAuth.find({}).catch(e=>{console.log(e)})
     console.log(tikToks)
     return <h1>Something</h1>
 }
