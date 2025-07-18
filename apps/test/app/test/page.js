605d80ce2e6187091540e90bd5998fb2efdf2bd7
Michael Thero <mthero24@gmail.com>
2025-07-18 07:25:58 -0400

    something

--- apps/test/app/test/page.js
+++ apps/test/app/test/page.js
@@ -2,9 +2,9 @@ import { getOrders, processOrders } from "@/functions/tikTok";
 import TikTokAuth from "@/models/tiktok";
 
 export default async function Test(){
-    let auths = await TikTokAuth.find({provider: "test"})
-    //console.log(auths);
-    let orders = await getOrders(auths)
-    processOrders(orders)
+    // let auths = await TikTokAuth.find({provider: "test"})
+    // //console.log(auths);
+    // let orders = await getOrders(auths)
+    // processOrders(orders)
     return <h1>test</h1>
 }
