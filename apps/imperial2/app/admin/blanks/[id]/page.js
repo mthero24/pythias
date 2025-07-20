3dc6d0e57cf7981eebaec94b84b5f47d4b681d89
Michael Thero <mthero24@gmail.com>
2025-07-20 18:05:59 -0400

    something

--- apps/imperial2/app/admin/blanks/[id]/page.js
+++ apps/imperial2/app/admin/blanks/[id]/page.js
@@ -3,7 +3,7 @@ import { serialize } from "@/functions/serialize";
 import {BlankMain as Main} from "@pythias/backend";
 export const dynamic = 'force-dynamic'; 
 export default async function Show(req, res){
-    //console.log(await req.params);
+    console.log(await req.params);
     let {id} = await req.params;
     let blank = await Blank.findById(id).populate("printLocations").lean()
     let blanks = await Blank.find({}).select("sizes").populate("colors").lean();
