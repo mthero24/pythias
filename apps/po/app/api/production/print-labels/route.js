015234345e3c08f3a6542363c44bea6248760ab3
Michael Thero <mthero24@gmail.com>
2025-07-23 17:47:35 -0400

    something

--- apps/po/app/api/production/print-labels/route.js
+++ apps/po/app/api/production/print-labels/route.js
@@ -9,10 +9,13 @@ import Inventory from "../../../../models/inventory";
 let letters = ["a", "b", "c", "d","e","f","g","h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 
 const subtractInventory = async (item)=>{
-    if(!item.type == "gift"){
+    console.log(item)
+    if(!item.type != "gift"){
         let inv = await Inventory.findOne({_id: item.inventory._id})
         //console.log(inv, "invetory subrtact")
-        inv.quantity -= 1
+        console.log(inv.quantity)
+        inv.quantity = inv.quantity - 1
+        console.log( inv.quantity, "qty")
         await inv.save()
         //console.log(inv, "invetory subrtact")
     }
@@ -52,9 +55,7 @@ export async function POST(req=NextApiRequest){
     }
     // full fill promises
     preLabels.map(l=> labelsString += l)
-    console.log(preLabels.length, "+++++++" ,preLabels)
     //create label string
-    console.log(labelsString)
     //convert to base64
     labelsString = btoa(labelsString)
    
