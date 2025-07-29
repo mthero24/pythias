ea9773168cfbd5d502a81bad72b94886e96c23bb
Michael Thero <mthero24@gmail.com>
2025-07-29 11:19:04 -0400

    something

--- apps/imperial2/app/test/page.js
+++ apps/imperial2/app/test/page.js
@@ -3,12 +3,12 @@ import {Inventory, InventoryOrders} from "@pythias/mongo"
 import axios from "axios";
 export default async function Test(){
     //await pullOrders()
-    let res = await axios.get(`https://www.etsy.com/oauth/connect?
-        response_type = code
-        & redirect_uri=https://www.pythiastechinologies.com/etsy
-    & scope=transactions_r transactions_w
-    & client_id=1aa2bb33c44d55eeeeee6fff & state=superstate
-    & code_challenge=DSWlW2Abh - cf8CeLL8 - g3hQ2WQyYdKyiu83u_s7nRhI
-    & code_challenge_method=S256`)
+    // let res = await axios.get(`https://www.etsy.com/oauth/connect?
+    //     response_type = code
+    //     & redirect_uri=https://www.pythiastechinologies.com/etsy
+    // & scope=transactions_r transactions_w
+    // & client_id=1aa2bb33c44d55eeeeee6fff & state=superstate
+    // & code_challenge=DSWlW2Abh - cf8CeLL8 - g3hQ2WQyYdKyiu83u_s7nRhI
+    // & code_challenge_method=S256`)
     return <h1>test</h1>
 }
