fc69bec829ecfb443befdcf0c828f2727ff3aeee
Michael Thero <mthero24@gmail.com>
2025-07-30 17:08:20 -0400

    something

--- apps/premier-printing/app/api/admin/products/route.js
+++ apps/premier-printing/app/api/admin/products/route.js
@@ -63,20 +63,13 @@ export async function POST(req = NextApiRequest) {
     }
     let products = [];
     for(let product of data.products) {
-        let variantsArray = []
+        // let variantsArray = []
         // if(product.threadColor && product.threadColors.length > 0){
         //     for (let b of product.blanks) {
         //         for (let tc of product.threadColors) {
         //             for (let c of product.colors) {
         //                 if (product.variants[b.code] && product.variants[b.code][tc.name] && product.variants[b.code][tc.name][c.name] && product.variants[b.code][tc.name][c.name].length > 0) {
-        //                     let variants = product.variants[b.code][tc.name][c.name].map(v => {
-        //                         v.color = v.color._id
-        //                         v.blank = v.blank._id
-        //                         v.size = v.size._id
-        //                         v.images = v.images.map(i=> i.image)
-        //                         return v;
-        //                     })
-        //                     variantsArray = [...variantsArray, ...variants];
+        //                    console.log("Found variants", product.variants[b.code][tc.name][c.name]);
         //                 }
         //             }
         //         }
@@ -85,22 +78,14 @@ export async function POST(req = NextApiRequest) {
         //     for (let b of product.blanks) {
         //         for (let c of product.colors) {
         //             if (product.variants[b.code] && product.variants[b.code][c.name] && product.variants[b.code][c.name].length > 0) {
-        //                 let variants = product.variants[b.code][c.name].map(v=> {
-        //                     v.color = c._id
-        //                     v.blank = b._id
-        //                     v.size = v.size._id
-        //                     v.images = v.images.map(i => i.image)
-        //                     return v;
-        //                 })
-        //                 variantsArray = [...variantsArray, ...variants];
+        //                 console.log("Found variants", product.variants[b.code][c.name]);
         //             }
         //         }
         //     }    
         // }
-        product.variantsArray = variantsArray
-        console.log(product.variantsArray)
+        // console.log(product.variants)
         if(product._id) {
-            product = await Products.findByIdAndUpdate(product._id, product, { new: true, returnNewDocument: true}).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors").populate({path:"blanks", populate: "colors"});
+            product = await Products.findByIdAndUpdate(product._id, {...product}, { new: true, returnNewDocument: true}).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors").populate({path:"blanks", populate: "colors"});
         }else{
             product = await Products.create(product)
             product = await Products.findById(product._id).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors").populate({path:"blanks", populate: "colors"});
--- apps/premier-printing/app/test/page.js
+++ apps/premier-printing/app/test/page.js
@@ -8,58 +8,58 @@ let converter = {
     YXXL: "2XL",
 }
 export default async function Test(){
-    let prods = await Products.find({}).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors variantsArray.color variantsArray.blank").populate({ path: "blanks", populate: "colors" })
-    let products = [];
-    for (let product of prods) {
-        let variantsArray = []
-        if (product.threadColor && product.threadColors.length > 0) {
-            for (let b of product.blanks) {
-                for (let tc of product.threadColors) {
-                    for (let c of product.colors) {
-                        if (product.variant && product.variants[b.code] && product.variants[b.code][tc.name] && product.variants[b.code][tc.name][c.name] && product.variants[b.code][tc.name][c.name].length > 0) {
-                            let variants = product.variants[b.code][tc.name][c.name].map(v => {
-                                v.color = v.color._id
-                                v.blank = v.blank._id
-                                v.size = v.size._id
-                                v.images = v.images.map(i => i.image ? i.image : i)
-                                return v;
-                            })
-                            variantsArray = [...variantsArray, ...variants];
-                        }
-                    }
-                }
-            }
-        } else {
-            for (let b of product.blanks) {
-                for (let c of product.colors) {
-                    if (product.variants && product.variants[b.code] && product.variants[b.code][c.name] && product.variants[b.code][c.name].length > 0) {
-                        let variants = product.variants[b.code][c.name].map(v => {
-                            console.log(v)
-                            v.color = v.color._id
-                            v.blank = v.blank._id
-                            v.size = v.size._id
-                            console.log(v.images)
-                            v.images = v.images?.map(i => i.image? i.image: i)
-                            return v;
-                        })
-                        variantsArray = [...variantsArray, ...variants];
-                    }
-                }
-            }
-        }
-        if(variantsArray.length > 0) product.variantsArray = variantsArray
-        if(variantsArray.length > 0){
-            //product.variants = null
-        }
-        console.log(product.variantsArray)
-        if (product._id) {
-            product = await Products.findByIdAndUpdate(product._id, product, { new: true, returnNewDocument: true }).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors").populate({ path: "blanks", populate: "colors" });
-        } else {
-            product = await Products.create(product)
-            product = await Products.findById(product._id).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors").populate({ path: "blanks", populate: "colors" });
-        }
-        products.push(product);
-    }
+    // let prods = await Products.find({}).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors variantsArray.color variantsArray.blank").populate({ path: "blanks", populate: "colors" })
+    // let products = [];
+    // for (let product of prods) {
+    //     let variantsArray = []
+    //     if (product.threadColor && product.threadColors.length > 0) {
+    //         for (let b of product.blanks) {
+    //             for (let tc of product.threadColors) {
+    //                 for (let c of product.colors) {
+    //                     if (product.variant && product.variants[b.code] && product.variants[b.code][tc.name] && product.variants[b.code][tc.name][c.name] && product.variants[b.code][tc.name][c.name].length > 0) {
+    //                         let variants = product.variants[b.code][tc.name][c.name].map(v => {
+    //                             v.color = v.color._id
+    //                             v.blank = v.blank._id
+    //                             v.size = v.size._id
+    //                             v.images = v.images.map(i => i.image ? i.image : i)
+    //                             return v;
+    //                         })
+    //                         variantsArray = [...variantsArray, ...variants];
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     } else {
+    //         for (let b of product.blanks) {
+    //             for (let c of product.colors) {
+    //                 if (product.variants && product.variants[b.code] && product.variants[b.code][c.name] && product.variants[b.code][c.name].length > 0) {
+    //                     let variants = product.variants[b.code][c.name].map(v => {
+    //                         console.log(v)
+    //                         v.color = v.color._id
+    //                         v.blank = v.blank._id
+    //                         v.size = v.size._id
+    //                         console.log(v.images)
+    //                         v.images = v.images?.map(i => i.image? i.image: i)
+    //                         return v;
+    //                     })
+    //                     variantsArray = [...variantsArray, ...variants];
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     if(variantsArray.length > 0) product.variantsArray = variantsArray
+    //     if(variantsArray.length > 0){
+    //         //product.variants = null
+    //     }
+    //     console.log(product.variantsArray)
+    //     if (product._id) {
+    //         product = await Products.findByIdAndUpdate(product._id, product, { new: true, returnNewDocument: true }).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors").populate({ path: "blanks", populate: "colors" });
+    //     } else {
+    //         product = await Products.create(product)
+    //         product = await Products.findById(product._id).populate("design colors productImages.blank productImages.color productImages.threadColor threadColors").populate({ path: "blanks", populate: "colors" });
+    //     }
+    //     products.push(product);
+    //}
 
     return <h1>test</h1>
 }
