7df0d1c28952c5dbbca873c0026c0154df3b91d2
Michael Thero <mthero24@gmail.com>
2025-08-04 15:16:54 -0400

    something

--- apps/premier-printing/app/api/admin/integrations/route.js
+++ apps/premier-printing/app/api/admin/integrations/route.js
@@ -4,6 +4,7 @@ import { ApiKeyIntegrations, TikTokAuth } from "@pythias/mongo";
 import next from "next";
 export async function GET(req=NextApiRequest){
     try{
+        let tiktok = await TikTokAuth.find({provider: req.nextUrl.searchParams.get("provider")});
         let integration = await ApiKeyIntegrations.find({ provider: req.nextUrl.searchParams.get("provider") });
         console.log("Integration found:", integration);
         return NextResponse.json({error: false, integration})
