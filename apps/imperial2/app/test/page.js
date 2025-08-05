8d2ef8c515dbcb7e845d58ca4365e3f7e6748b22
Michael Thero <mthero24@gmail.com>
2025-08-05 11:25:50 -0400

    something

--- apps/imperial2/app/test/page.js
+++ apps/imperial2/app/test/page.js
@@ -3,15 +3,15 @@ import {Inventory, InventoryOrders, Products} from "@pythias/mongo"
 import axios from "axios";
 export default async function Test(){
 
-    let prods = await Products.find({})
+    // let prods = await Products.find({})
 
-    for(let p of prods){
-        for(let v of p.variantsArray){
-            v.inventory = await Inventory.findOne({ blank: v.blank, color: v.color, sizeId: v.size });
-        }
-        p.markModified("variantsArray");
-        await p.save();
-    }
+    // for(let p of prods){
+    //     for(let v of p.variantsArray){
+    //         v.inventory = await Inventory.findOne({ blank: v.blank, color: v.color, sizeId: v.size });
+    //     }
+    //     p.markModified("variantsArray");
+    //     await p.save();
+    // }
 
     return <h1>test</h1>
 }
