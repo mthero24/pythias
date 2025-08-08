ea0c615c6d028089946182bceb984320cdb911d9
Michael Thero <mthero24@gmail.com>
2025-08-08 16:05:57 -0400

    something

--- apps/imperial2/app/admin/blanks/page.js
+++ apps/imperial2/app/admin/blanks/page.js
@@ -3,7 +3,6 @@ import { Blank } from "@pythias/mongo";
 export const dynamic = 'force-dynamic'; 
 export default async function Blanks(){
     let blanks = await Blank.find({}).select("code name vendor department sales _id").lean().catch(e=>{console.log(e)});
-    console.log(blanks)
     if(blanks)blanks = JSON.parse(JSON.stringify(blanks))
     else blanks = []
     return (
