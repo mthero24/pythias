7c59ccfc0514aa81e8cb5f6d9abead1769c07b74
Michael Thero <mthero24@gmail.com>
2025-08-08 15:20:20 -0400

    something

--- apps/imperial2/app/admin/page.js
+++ apps/imperial2/app/admin/page.js
@@ -2,6 +2,7 @@ import {Box} from "@mui/material"
 export default function Admin(){
     return(<Box>
             <h1 style={{fontSize: "3rem", textAlign: "center"}}>Admin Page</h1>
+            {console.log("Loading admin page")}
             <Box sx={{margin: "2%"}}>
                 <iframe style={{background: "#FFFFFF", border: "none",borderRadius: "2px",boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)" }}width="100%" height="480" src="https://charts.mongodb.com/charts-pythias-tech-ckbsbme/embed/charts?id=d45c5555-2b2f-480b-87a0-738b23c5055e&maxDataAge=3600&theme=light&autoRefresh=true"></iframe>
             </Box>
