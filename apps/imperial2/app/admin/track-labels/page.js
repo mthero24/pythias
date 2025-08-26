a59725e9cd54ec714a2aa2ed270c5319f8893957
Michael Thero <mthero24@gmail.com>
2025-08-25 20:05:58 -0400

    something

--- apps/imperial2/app/admin/track-labels/page.js
+++ apps/imperial2/app/admin/track-labels/page.js
@@ -4,7 +4,7 @@ import {TrackLabels as TrackLabelsComponent, serialize} from "@pythias/backend";
 
 export default async function TrackLabels(){
     let items = await Item.find({labelPrinted: true, shipped: false, canceled: false}).populate("order", "poNumber status date preShipped notes").sort({_id: 1});
-    console.log(items.length, "items.length")
+    //console.log(items.length, "items.length")
     // for(let item of items){
     //     if(item.order == undefined || item.order.poNumber == undefined) {
     //         item.canceled = true;
