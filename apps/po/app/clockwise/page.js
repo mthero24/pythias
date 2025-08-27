611f6377b78d43cd8feea518c98b7f0ad744e56b
Michael Thero <mthero24@gmail.com>
2025-08-27 08:29:10 -0400

    something

--- apps/po/app/clockwise/page.js
+++ apps/po/app/clockwise/page.js
@@ -2,6 +2,8 @@ import Order from "@/models/Order";
 import {Main}from "./Main";
 
 export default async function Page(){
+    console.log("Clockwise Page Render")
+
     return (
         <Main orders={{}}/>
     )
