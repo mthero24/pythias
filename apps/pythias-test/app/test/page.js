5dbc9507a09ed402f14a4a463e2b04d612b11444
Michael Thero <mthero24@gmail.com>
2025-08-27 02:45:36 -0400

    something

--- apps/pythias-test/app/test/page.js
+++ apps/pythias-test/app/test/page.js
@@ -12,28 +12,28 @@ let converter = {
 }
 export default async function Test(){
     
-    let connection = await ApiKeyIntegrations.findOne({provider: "pythias-test", type: "etsy"})
-    // console.log(connection, "connection")
-    // let newConnection = await refreshToken(connection.refreshToken)
-    // connection.apiKey = newConnection.access_token
-    // connection.refreshToken = newConnection.refresh_token
-    // await connection.save()
-    const requestOptions = {
-        headers: {
-            "x-api-key": "480pxuspxi5wz93puk47snye",
-        },
-    };
-    let url = `https://openapi.etsy.com/v3/application/shops?shop_name=PythiaShop`;
-    let response = await axios.get(
-        url,
-        {
-            headers: {
-                "x-api-key": "480pxuspxi5wz93puk47snye",
-                Authorization: `Bearer ${connection.apiKey}`,
-            },
-        }
-    ).catch(e=>console.log(e.response.data));
+    // let connection = await ApiKeyIntegrations.findOne({provider: "pythias-test", type: "etsy"})
+    // // console.log(connection, "connection")
+    // // let newConnection = await refreshToken(connection.refreshToken)
+    // // connection.apiKey = newConnection.access_token
+    // // connection.refreshToken = newConnection.refresh_token
+    // // await connection.save()
+    // const requestOptions = {
+    //     headers: {
+    //         "x-api-key": "480pxuspxi5wz93puk47snye",
+    //     },
+    // };
+    // let url = `https://openapi.etsy.com/v3/application/shops?shop_name=PythiaShop`;
+    // let response = await axios.get(
+    //     url,
+    //     {
+    //         headers: {
+    //             "x-api-key": "480pxuspxi5wz93puk47snye",
+    //             Authorization: `Bearer ${connection.apiKey}`,
+    //         },
+    //     }
+    // ).catch(e=>console.log(e.response.data));
 
-    console.log(response?.data);
+    // console.log(response?.data);
     return <h1>test</h1>
 }
