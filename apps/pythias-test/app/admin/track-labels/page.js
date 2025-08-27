4c1ed8221323b230ba37c3aa820a4bf600289021
Michael Thero <mthero24@gmail.com>
2025-08-27 02:29:35 -0400

    something

--- apps/pythias-test/app/admin/track-labels/page.js
+++ apps/pythias-test/app/admin/track-labels/page.js
@@ -3,7 +3,12 @@ import {TrackLabels as TrackLabelsComponent, serialize} from "@pythias/backend";
 
 
 export default async function TrackLabels(){
-    let items = await Item.find({labelPrinted: true, shipped: false, canceled: false}).populate("designRef order").sort({_id: 1}).lean();
-    items = serialize(items)
-    return <TrackLabelsComponent items={items} source="imperial" />;
+    try{
+        let items = await Item.find({labelPrinted: true, shipped: false, canceled: false}).populate("designRef order").sort({_id: 1}).lean();
+        items = serialize(items)
+        return <TrackLabelsComponent items={items} source="test" />;
+    }catch(e){
+        console.log(e)
+        return <TrackLabelsComponent items={[]} source="test" />;
+    }
 }
