f169b66b4373da21cbcffe4d7a6da34608ca4c85
Michael Thero <mthero24@gmail.com>
2025-08-28 18:13:55 -0400

    something

--- apps/pythias-test/app/account/page.js
+++ apps/pythias-test/app/account/page.js
@@ -5,7 +5,6 @@ import { serialize } from "@/functions/serialize";
 import {headers} from "next/headers"
 export default async function Account(req){
     //why wond you update the user?
-    console.log("Account page")
     const headersList = await headers()
     console.log(headersList.get("user"))
     let user = await User.findOne({userName: headersList.get("user")})
