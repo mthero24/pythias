cf26a80a1a3b0983a7dd88d6ddcbe4acb5a3fa81
Michael Thero <mthero24@gmail.com>
2025-10-02 14:05:08 -0400

    something

--- apps/po/app/layout.js
+++ apps/po/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NavBar from "@/components/Navbar2"
+import NavBar from "@/components/Navbar"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
