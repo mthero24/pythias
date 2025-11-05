f814b4c430fd11a4d83e90bd6492d04570101a8b
Michael Thero <mthero24@gmail.com>
2025-11-05 00:12:34 -0500

    something

--- apps/premier-printing/app/api/admin/inventory/create-order/add/route.js
+++ apps/premier-printing/app/api/admin/inventory/create-order/add/route.js
@@ -2,6 +2,7 @@ import { NextApiRequest, NextResponse } from "next/server"
 import { Inventory  } from "@pythias/mongo";
 
 export async function POST(req = NextApiRequest) {
+    console.log("hit")
     let data = await req.json()
     console.log(data)
     let inventory = await Inventory.findOne({ blank: data.blank._id, color: data.color._id, sizeId: data.size._id })
