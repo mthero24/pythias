abbe5e00dd9281af2e78f8b87d9b033b4398b44a
Michael Thero <mthero24@gmail.com>
2025-11-06 08:41:00 -0500

    something

--- apps/po/app/bulk/page.js
+++ apps/po/app/bulk/page.js
@@ -1,6 +1,7 @@
 import Order from "../../models/Order";
 import {serialize} from "@pythias/backend";
 import {BulkMain} from "@pythias/labels";
+export const dynamic = 'force-dynamic';
 export default async function Bulk(){
     let orders = await Order.find({bulk: true, status: {$nin: ["canceled", "returned", "shipped", "Shipped", "delivered"]}}).populate({path: "items", populate: {path: "inventory.inventory"}}).lean();
     orders = serialize(orders)
