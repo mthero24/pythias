6347d0d7faf2d4c3b248c05e456ad045a684050d
Michael Thero <mthero24@gmail.com>
2026-01-05 08:24:20 -0500

    something

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       script: "node_modules/next/dist/bin/next", // Use Next.js binary
       args: "start -p 3000", // Start on port 3000
       exec_mode: "cluster", // Enable cluster mode to use multiple CPUs
-      instances: "10", // Use all available CPU cores
+      instances: "5", // Use all available CPU cores
       env: {
         NODE_ENV: "production", // Set production environment
       },
@@ -98,7 +98,7 @@ module.exports = {
       script: "node_modules/next/dist/bin/next", // Use Next.js binary
       args: "start -p 3001", // Start on port 3000
       exec_mode: "cluster", // Enable cluster mode to use multiple CPUs
-      instances: "3", // Use all available CPU cores
+      instances: "4", // Use all available CPU cores
       env: {
         NODE_ENV: "production", // Set production environment
       },
