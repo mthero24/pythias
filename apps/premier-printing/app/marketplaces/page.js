0af62fb975dbd318d4ab4f5002f698e3f0a2045a
Michael Thero <mthero24@gmail.com>
2026-03-17 16:08:33 -0400

    something

--- apps/premier-printing/app/marketplaces/page.js
+++ apps/premier-printing/app/marketplaces/page.js
@@ -4,7 +4,7 @@ import { MarketplacesMain, serialize } from "@pythias/backend";
 export const metadata = {
     title: 'Marketplaces - Premier Printing',
 };
-
+export const dynamic = 'force-dynamic';
 export default async function handler(req, res) {
     let marketplaces = await MarketPlaces.find().lean();
     marketplaces = serialize(marketplaces);
