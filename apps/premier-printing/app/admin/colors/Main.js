fe1abb8c164332de7fdb7a26e5846221c4375083
Michael Thero <mthero24@gmail.com>
2026-04-10 12:59:25 -0400

    something

--- apps/premier-printing/app/admin/colors/Main.js
+++ apps/premier-printing/app/admin/colors/Main.js
@@ -98,7 +98,7 @@ export function DeleteModal({ open, setOpen, color, setColors }) {
         <Modal open={open} onClose={() => setOpen(false)} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
             <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', boxShadow: 24, p: 4, }}>
                 <Typography id="modal-modal-title" variant="h6" component="h2">Delete Color</Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>Are you sure you want to delete {color.name}?</Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>Are you sure you want to delete {color?.name}?</Typography>
                 <Box sx={{ display: "flex", justifyContent: "flex-end", marginTop: 3 }}>
                     <Button variant="contained" color="error" onClick={handleDelete} sx={{ marginRight: 2 }}>Delete</Button>
                     <Button variant="outlined" onClick={() => setOpen(false)}>Cancel</Button>
