a927286366383f929def23cbcb652a02a3b65a89
Michael Thero <mthero24@gmail.com>
2026-06-03 01:11:18 -0400

    target download

--- apps/pythias-platform/next.config.mjs
+++ apps/pythias-platform/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverExternalPackages: ["@pythias/mongo", "@pythias/sublimation", "sharp", "canvas", "pdfkit", "@napi-rs/canvas", "@pythias/shipping"],
+    serverExternalPackages: ["@pythias/mongo", "@pythias/sublimation", "sharp", "canvas", "pdfkit", "@napi-rs/canvas", "@pythias/shipping", "@pythias/dtf"],
     transpilePackages: ["@pythias/backend", "@pythias/labels", "@pythias/roq-folder"],
     images: {
         remotePatterns: [
